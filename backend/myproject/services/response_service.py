679d684a24f8da445e111e3e344a6a80d9725fbd
O'Donnell Tomagan <voophdev@Donnell-2.local>
2025-05-25 05:38:00 +0800

    Updated response service

--- backend/myproject/services/response_service.py
+++ backend/myproject/services/response_service.py
@@ -1,42 +1,42 @@
 from myproject.utils.response_service_util import InternalError, NotFound, BadRequest
 from rest_framework.response import Response
-from rest_framework.request import Request
 from rest_framework import status
-from datetime import datetime, timedelta
-from typing import List, Dict, Tuple, Optional
-from django.utils import timezone
 
 
 class ResponseService:
 
-    def client_response(self, status_code: int, response_data: any = None) -> Response:
+    @classmethod
+    def client_response(cls, status_code: int, response_data: any = None) -> Response:
 
         if response_data == None:
             return Response(status=status_code)
 
         return Response(response_data, status=status_code)
 
-    def ok(self, response_data: any) -> Response:
+    @classmethod
+    def ok(cls, response_data: any) -> Response:
         """Returns 200 status code."""
 
-        return self.client_response(
+        return cls.client_response(
             status_code=status.HTTP_200_OK,
             response_data=response_data
         )
 
-    def created(self, response_data: any) -> Response:
+    @classmethod
+    def created(cls, response_data: any) -> Response:
         """Returns 201 status code."""
-        return self.client_response(
+        return cls.client_response(
             status_code=status.HTTP_201_CREATED,
             response_data=response_data
         )
 
-    def no_content(self, ) -> Response:
-        return self.client_response(
+    def no_content(cls, ) -> Response:
+        return cls.client_response(
             status_code=status.HTTP_204_NO_CONTENT
         )
 
-    def internal_error(self, exception: Exception,
+    @classmethod
+    def internal_error(cls, exception: Exception,
                        error_message: str | None = None, error_details: list[any] | None = None) -> Response:
         """Returns 500 status code."""
 
@@ -51,31 +51,32 @@ class ResponseService:
         error_message_obj = InternalError(
             message=error_message, details=res_error_details)
 
-        return self.client_response(
+        return cls.client_response(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             response_data=error_message_obj.to_json()
         )
 
-    def not_found(self, error_details: list[any], error_message: str | None = None) -> Response:
+    @classmethod
+    def not_found(cls, error_details: list[any], error_message: str | None = None) -> Response:
         """Returns 404 status code."""
 
         error_message = error_message if error_message != None else "Not Found"
         error_message_obj = NotFound(
             message=error_message, details=error_details)
 
-        return self.client_response(
+        return cls.client_response(
             status_code=status.HTTP_404_NOT_FOUND,
             response_data=error_message_obj.to_json()
         )
 
-    def bad_request(self, error_details: list[any], error_message: str | None = None) -> Response:
+    @classmethod
+    def bad_request(cls, error_details: list[any], error_message: str | None = None) -> Response:
         """Returns 400 status code."""
 
         error_message = error_message if error_message != None else "Bad Request"
-        print("BAD REQUEST BEFORE")
         error_message_obj = BadRequest(
             message=error_message, details=error_details)
-        return self.client_response(
+        return cls.client_response(
             status_code=status.HTTP_400_BAD_REQUEST,
             response_data=error_message_obj.to_json()
         )
